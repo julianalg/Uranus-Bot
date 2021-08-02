fcb0e42ca87b0507a9fc5f0eb9441f04093c40e5
Julian Garcia <69781917+julianagar@users.noreply.github.com>
2021-08-01 21:53:52 -0700

    Add files via upload

---
+++ recommedationlists.py
@@ -0,0 +1,22 @@
+# recommendations kept here and not in bot.py so it doesnt fill up the whole thing
+
+
+# movies
+romanceMovies = ['Crounching Tiger, Hidden Dragon: https://bit.ly/3inx1Wx', 'Call Me by Your Name: https://imdb.to/3xjfsL6']
+dramaMovies = []
+animatedMovies = []
+
+# shows 
+romanceShows = []
+comedyShows = ['Tuca & Bertie: https://imdb.to/3ydQ8HT']
+dramaShows = []
+
+# video games
+narrativeGames = []
+adventureGames = []
+puzzleGames = []
+
+# music genres 
+rockMusic = ['day6: https://bit.ly/3ig3cH6']
+popMusic = []
+indieMusic = []
