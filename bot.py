9d3dc38a500ab0edbe52b52225156e75a8b7f608
Julian Garcia <69781917+julianagar@users.noreply.github.com>
2021-08-15 23:43:53 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,9 +21,9 @@ playlists = ['https://open.spotify.com/playlist/283WZvKuACszj3K0xyId4V?si=pqjTqh
 # among us
 amongUs = ['https://tenor.com/view/among-us-twerk-yellow-ass-thang-gif-18983570', 'https://imgur.com/a/VMmL45B', 'https://imgur.com/a/o1tlY0O', 'https://imgur.com/a/x58bHsf', 'https://media.discordapp.net/attachments/872928261510934618/873011151527637032/JermaSus.webp?width=270&height=338', 'https://media.discordapp.net/attachments/872928261510934618/873011163506544691/Among-Us-memes.jpg?width=705&height=353', 'https://cdn.discordapp.com/attachments/872928261510934618/873011174860541982/gpue7yepssp51.jpg', 'https://cdn.discordapp.com/attachments/872928261510934618/873011187065970728/maxresdefault.jpg', 'https://cdn.discordapp.com/attachments/872928261510934618/873011199783096410/4jlqsm.jpg', 'https://cdn.discordapp.com/attachments/872928261510934618/873011263196766238/a6077ef30ba8d41d914549906e3b5533.jpg']
 # useless net
-uselessWebsites = ['https://www.hackertyper.com/', 'https://onefishstudio.net/ucg', 'https://pointerpointer.com/', 'https://www.boredbutton.com/', 'https://smashthewalls.com/', 'https://mondrianandme.com', 'https://thatsthefinger.com', 'https://puginarug.com', 'https://cant-not-tweet-this.com', 'https://alwaysjudgeabookbyitscover.com',  'https://www.mapcrunch.com/', 'https://www.window-swap.com/Window', 'https://screamintothevoid.com/', 'https://archive.org/details/msdos_Oregon_Trail_The_1990']
+uselessWebsites = ['https://www.hackertyper.com/', 'https://onefishstudio.net/ucg', 'https://pointerpointer.com/', 'https://www.boredbutton.com/', 'https://smashthewalls.com/', 'https://mondrianandme.com', 'https://thatsthefinger.com', 'https://puginarug.com', 'https://cant-not-tweet-this.com', 'https://alwaysjudgeabookbyitscover.com',  'https://www.mapcrunch.com/', 'https://www.window-swap.com/Window', 'https://screamintothevoid.com/', 'https://archive.org/details/msdos_Oregon_Trail_The_1990', 'https://www.thesolaropposites.com/lessonalyzer/', 'https://nestflix.fun']
 # cute animal
-cuteAnimal = ['https://imgur.com/t/aww/EsVNl3y', 'https://imgur.com/t/aww/OYSoXGx', 'https://imgur.com/t/aww/EKKWq5Q', 'https://imgur.com/t/aww/hBkTtOT', 'https://imgur.com/t/aww/GWvmwBy', 'https://imgur.com/t/aww/cYOAzQG', 'https://imgur.com/t/aww/PYO3jHw', 'https://imgur.com/t/aww/onaQY4K', 'https://imgur.com/t/aww/9JqesLH', 'https://www.reddit.com/r/aww/comments/oyoksi/the_determined_doggo', 'https://www.reddit.com/r/aww/comments/oyjgtp/thats_too_cute_i_will_die_from_cuteness/']
+cuteAnimal = ['https://imgur.com/t/aww/EsVNl3y', 'https://imgur.com/t/aww/OYSoXGx', 'https://imgur.com/t/aww/EKKWq5Q', 'https://imgur.com/t/aww/hBkTtOT', 'https://imgur.com/t/aww/GWvmwBy', 'https://imgur.com/t/aww/cYOAzQG', 'https://imgur.com/t/aww/PYO3jHw', 'https://imgur.com/t/aww/onaQY4K', 'https://imgur.com/t/aww/9JqesLH', 'https://www.reddit.com/r/aww/comments/oyoksi/the_determined_doggo', 'https://www.reddit.com/r/aww/comments/oyjgtp/thats_too_cute_i_will_die_from_cuteness/', 'https://imgur.com/t/petshow2021/oVbdYj9', 'https://imgur.com/t/petshow2021/0ii4FJ5', 'https://imgur.com/t/petshow2021/NmBuPAe', 'https://imgur.com/t/petshow2021/GLKbglZ']
 # recommendations kept here and not in bot.py so it doesnt fill up the whole thing
 romanceMovies = ['Crounching Tiger, Hidden Dragon: https://bit.ly/3inx1Wx', 'Call Me by Your Name: https://imdb.to/3xjfsL6']
 animatedMovies = ['Luca: https://bit.ly/3yk6y1f', "Howl's Moving Castle: https://imdb.to/3jgHWQY", 'Your Name.: https://imdb.to/37khqk1', 'The Emoji Movie: https://imdb.to/3iqvOgW']
@@ -39,7 +39,7 @@ alternativeMuic = ['Joji: https://bit.ly/3A0Op9b', 'Rex Orange County: https://b
 # albums
 popAlbums = ['Happier than Ever: https://bit.ly/3yjMdJv', 'Planet Her: https://bit.ly/3jeaA5h', 'Hot Pink: https://music.youtube.com/playlist?list=OLAK5uy_nN6ToczJSrgaKwaWT3jyvNgedNcs2D94Y&feature=gws_kp_album&feature=gws_kp_artist', 'SOUR: https://music.youtube.com/playlist?list=OLAK5uy_mRc9HLtU_4lp_-MKQNApIRiLwY0M6xoCA&feature=gws_kp_album&feature=gws_kp_artist', 'Blood Harmony: https://music.youtube.com/playlist?list=OLAK5uy_mb0tj-3a2sG_iILVI1LC1jejNJDkTbDeg&feature=gws_kp_album&feature=gws_kp_artist']
 indieAlbums = ['Sling: https://bit.ly/3iksXpM', 'Jubilee: https://bit.ly/3jl9F34', 'Apricot Princess: https://music.youtube.com/playlist?list=OLAK5uy_n7pYtvKITUmxl51Y5Sv7IbU0mdxmskDw8&feature=gws_kp_album&feature=gws_kp_artist', 'Harmony House: https://music.youtube.com/playlist?list=OLAK5uy_nC-k8nCXlXiw_46lVDh5MpKEFs4E7ED_Q&feature=gws_kp_album&feature=gws_kp_artist']
-rnbAlbums = ['Ctrl: https://bit.ly/3fGqPan', "When It's All Said And Done... Take Time: https://bit.ly/3zX5il6"]
+rnbAlbums = ['Ctrl: https://bit.ly/3fGqPan', "When It's All Said And Done... Take Time: https://bit.ly/3zX5il6", 'After Hours: https://open.spotify.com/album/4yP0hdKOZPNshxUOjY0cZj?autoplay=true', 'Blond: https://open.spotify.com/album/3mH6qwIy9crq0I9YQbOuDf?autoplay=true']
 kpopAlbums = ['Regular-Irregular: https://bit.ly/3Arl6Nl', 'THE OTHER SIDE OF THE MOON: https://bit.ly/3fQkitx', 'She Is: https://bit.ly/2U4adS6', 'The Perfect Red Velvet: https://bit.ly/3xxvm4H', 'Feel Special: https://bit.ly/3lIyPvc', 'Remember Us: Youth Part 2: https://bit.ly/37sfCFP', 'FEVER: https://bit.ly/3jHujdE', 'Zero: Fever Part.1: https://music.youtube.com/playlist?list=OLAK5uy_mk8hALS6QQbsjFhXANAoT85cAP9wdMJfE&feature=gws_kp_album&feature=gws_kp_artist', "Don't Call Me: https://music.youtube.com/playlist?list=OLAK5uy_kPiZ5ftT-ifzoFmoYroH_WffozSAFm_eU&feature=gws_kp_album&feature=gws_kp_artist", 'BORDER: CARNIVAL: https://music.youtube.com/playlist?list=OLAK5uy_nxPYOoHgmm1iR-bs39vZNJa3rxr-zhCx8&feature=gws_kp_album&feature=gws_kp_artist', 'Teen, Age: https://music.youtube.com/playlist?list=OLAK5uy_k3LRlcnKy4EcWk1Joc5lKOSlP2-WYbrwY&feature=gws_kp_album&feature=gws_kp_artist', 'NCT-RESONACE,Pt.2 (The 2nd Album): https://music.youtube.com/playlist?list=OLAK5uy_kbSg7V8UK4ZDzPaGRfVJI7rsDP2LQR7Zg&feature=gws_kp_album&feature=gws_kp_artist', "Ex'Act: https://music.youtube.com/playlist?list=OLAK5uy_n0yuTw9cb1INJlnZ9ydDV-zRvbPi_iH4w&feature=gws_kp_album&feature=gws_kp_artist", 'Hello Future: https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwivoPPCmaXyAhUSDzQIHTS0AKQQyCkwAHoECAIQAw&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DQPUjV7epJqE&usg=AOvVaw3t4kqPEwDm6PSpGbjC_t6y', 'instinct:  https://music.youtube.com/watch?v=aOuK3WUffXc&feature=gws_kp_track', 'QUERENCIA: https://music.youtube.com/playlist?list=OLAK5uy_lt7o5tYOgtUF8098LlJxwWoMaHRqwxfDg&feature=gws_kp_album&feature=gws_kp_artist', '130 mood: TRBL: https://music.youtube.com/playlist?list=OLAK5uy_nF3pW60THeSSuC-IdFfQh2wautFpKWRSo&feature=gws_kp_album&feature=gws_kp_artist', 'IS ANYBODY OUT THERE?: https://music.youtube.com/playlist?list=OLAK5uy_nY9vJNOiFsuUe4vsinqpgy3lnk8ykB7lE&feature=gws_kp_album&feature=gws_kp_artist']
 comedyAlbums = ['Bo Burnham: Inside (The Songs): https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjgmMmx9KbyAhUS3p4KHY8ZBO0QFnoECAMQAQ&url=https%3A%2F%2Fopen.spotify.com%2Falbum%2F1e5OlE0EY5fucq6GIU1xi3&usg=AOvVaw3BqZQ22RGQIRg1cWTdhq3v', 'New In Town: https://open.spotify.com/album/3ZvULmHESu5iaRNSAIewQ0?autoplay=true']
 # jokes 
@@ -49,10 +49,7 @@ csResponseQuestions = ['ask L instead', 'therapy is always an option', 'maybe if
 # tasks
 tasks = ['clean bedroom', 'clear out email inbox', 're-do phone home screen', 'change your wallpapers (tip: ?wallpaper)', 'go on a walk or stretch', 'say good morning/afternoon/evening/night or check up on a friend', "sort and sell things you don't need", "contribute ideas to the bot (?submit)"]
 # tips
-tips = ['?rate: have the bot rate anything', '?cock: :zany_face:', '?dance: dancing cat', '?ask carl: ask carl suburbs something', '?wp: wallpaper', '?recommendations: recommendations']
-# random number
-cuzRandintDoesntWorkIHateThis = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
-
+tips = ['?rate: have the bot rate anything', '?cock: :zany_face:', '?dance: dancing cat', '?ask carl: ask carl suburbs something', '?wp: wallpaper', '?recommendations: recommendations', '? void shout: shout random messages into the void', '?void pull: pull random messages out of the void', '?cute: cute animals', '?among us: im sorry', "?useless: useless website", "?task: a productive task todo"]
 
 shouts = []
 
@@ -99,42 +96,28 @@ async def on_message(message):
         await message.channel.send('`ノ ) 　 Lﾉ')
         
     if message.content.startswith('?ask carl'):
-          randomCarl = random.choice(csResponseQuestions)
-          await message.channel.send(randomCarl)
+          await message.channel.send(random.choice(csResponseQuestions))
         
     if message.content == '?playlist':
-      randomPlaylist = random.choice(playlists)
-      await message.channel.send(randomPlaylist)
+      await message.channel.send(random.choice(playlists))
   
     if message.content == '?among us': # from dj
-      randomAmogUs = random.choice(amongUs)
-      await message.channel.send(randomAmogUs)
+      await message.channel.send(random.choice(amongUs))
     
     if message.content == '?useless':
-      randomUseless = random.choice(uselessWebsites)
-      await message.channel.send(randomUseless)
+      await message.channel.send(random.choice(uselessWebsites))
   
     if message.content == '?cute':
-      randomCute = random.choice(cuteAnimal)
-      await message.channel.send(randomCute)
+      await message.channel.send(random.choice(cuteAnimal))
   
     if message.content == '?jokes':
-      randomJoke = random.choice(jokes)
-      await message.channel.send(randomJoke)
+      await message.channel.send(random.choice(jokes))
       
     if message.content == '?task':
-      randomTasks = random.choice(tasks)
-      await message.channel.send(randomTasks)
-
+      await message.channel.send(random.choice(tasks))
      
-    if message.content.startswith('?number rate'):
-        if message.content == '?number rate adam':
-          await message.channel.send('101/100')
-        if message.content == "?number rate rina's love for jaymes":
-          await message.channel.send('-1/100')
-        else:
-          rateNumber = randint(0, 100)
-          await message.channel.send(str(rateNumber) + '/100')
+    if message.content.startswith("?number rate"):
+      await message.channel.send(str(randint(0,100)) + '/100')
 
     # void and repeating messages
 
@@ -163,36 +146,28 @@ async def on_message(message):
         await message.channel.send('do "?wp (then the catergory you want)"')
         
     if message.content == '?wp landscape':
-        randomLandscape = random.choice(landscapeWallpaper)
-        await message.channel.send(randomLandscape)
+        await message.channel.send(random.choice(landscapeWallpaper))
 
-    if message.content == '?wp apple':
-        randomApple = random.choice(appleWallpaper)
-        await message.channel.send(randomApple)
+    if message.content == '?wp apple': 
+        await message.channel.send(random.choice(appleWallpaper))
 
     if message.content == '?wp anime':
-        randomAnime = random.choice(animeWallpaper)
-        await message.channel.send(randomAnime)
+        await message.channel.send(random.choice(animeWallpaper))
 
     if message.content == '?wp cityscape':
-        randomCityscape = random.choice(cityWallpper)
-        await message.channel.send(randomCityscape)
+        await message.channel.send(random.choice(cityWallpper))
 
     if message.content == '?wp space':
-        randomSpace = random.choice(spaceWallpaper)
-        await message.channel.send(randomSpace)
+        await message.channel.send(random.choice(spaceWallpaper))
 
     if message.content == '?wp color':
-        randomColor = random.choice(colorWallpaper)
-        await message.channel.send(randomColor)
+        await message.channel.send(random.choice(colorWallpaper))
 
     if message.content == '?wp pride':
-        randomPride = random.choice(prideWallpaper)
-        await message.channel.send(randomPride)
+        await message.channel.send(random.choice(prideWallpaper))
         
     if message.content == '?wp illustration':
-        randomllustration = random.choice(illustrationWallpaper)
-        await message.channel.send(randomllustration)
+        await message.channel.send(random.choice(illustrationWallpaper))
         
     # recommendation cluster fuck
     
@@ -205,72 +180,59 @@ async def on_message(message):
         await message.channel.send('do ?movie (catergory)')
             
     if message.content == '?movie romance':
-        randomRomance = random.choice(romanceMovies)
-        await message.channel.send(randomRomance)
+        await message.channel.send(random.choice(romanceMovies))
         
     if message.content == '?movie animated':
-        randomAnimated = random.choice(animatedMovies)
-        await message.channel.send(randomAnimated)
+        await message.channel.send(random.choice(animatedMovies))
               
     if message.content == '?tv show':
         await message.channel.send('which would you like: romance, comedy, drama, anime')
         await message.channel.send('do ?tv (catergory)')
           
     if message.content == '?tv comedy':
-        randomComedyTV = random.choice(comedyShows)
-        await message.channel.send(randomComedyTV)
+        await message.channel.send(random.choice(comedyShows))
             
-    if message.content == '?tv cartoon':
-        randomCartoon = random.choice(cartoonShows)
-        await message.channel.send(randomCartoon)
+    if message.content == '?tv cartoon': 
+        await message.channel.send(random.choice(cartoonShows))
 
     if message.content == '?tv anime':
-      randomAnime = random.choice(animeShows)
-      await message.channel.send(randomAnime)
+      await message.channel.send(random.choice(animeShows))
 
     if message.content == '?tv drama':
-      randomDrama = random.choice(dramaShows)
-      await message.channel.send(randomDrama)
+      await message.channel.send(random.choice(dramaShows))
 
     if message.content == '?artist':
       await message.channel.send('which would you like: rock or pop?')
       await message.channel.send('do ?artist (catergory)')
 
     if message.content == '?artist rock':
-      randomRock = random.choice(rockMusic)
-      await message.channel.send(randomRock)
+      await message.channel.send(random.choice(rockMusic))
         
     if message.content == '?artist alternative':
-      randomAlt = random.choice(alternativeMuic)
-      await message.channel.send(randomAlt)
+      await message.channel.send(random.choice(alternativeMuic))
           
     if message.content == '?album':
       await message.channel.send('which would you like: pop, indie, kpop, comedy, or R&B?')
       await message.channel.send('do ?album (catergory)')
         
     if message.content == '?album pop':
-      randomPop = random.choice(popAlbums)
-      await message.channel.send(randomPop)
+      await message.channel.send(random.choice(popAlbums))
           
     if message.content == '?album indie':
-      randomIndie = random.choice(indieAlbums)
-      await message.channel.send(randomIndie)
+      await message.channel.send(random.choice(indieAlbums))
     
     if message.content == '?album kpop':
-      randomkPop = random.choice(kpopAlbums)
-      await message.channel.send(randomkPop)
+      await message.channel.send(random.choice(kpopAlbums))
     
     if message.content == '?album comedy':
       await message.channel.send(random.choice(comedyAlbums))
       
     if message.content == '?album R&B':
-      randomRNB = random.choice(rnbAlbums)
-      await message.channel.send(randomRNB)
+      await message.channel.send(random.choice(rnbAlbums))
       
       
     # bot info and other stuff
     
-        
     if message.content == '?submit':
       await message.channel.send('submit stuff here: https://forms.gle/jz4bemZKgjj4d1oWA')
           
@@ -287,8 +249,7 @@ async def on_message(message):
       await message.channel.send('view the changelog here: https://github.com/julianagar/Poop-Bot/blob/main/changelog.md')
       
     if message.content == '?tip':
-      randomTip = random.choice(tips)
-      await message.channel.send(randomTip)
+      await message.channel.send(random.choice(tips))
 
 # more bot running stuff 
       
