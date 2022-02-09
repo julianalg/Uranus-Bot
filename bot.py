7d168ab7dfd75b3876d02734c28064aed330c2e7
Julian Garcia <69781917+julianagar@users.noreply.github.com>
2022-02-09 07:29:54 -0800

    updates to time machine

--- bot.py
+++ bot.py
@@ -41,28 +41,36 @@ async def on_message(message):
         await message.channel.send('https://lmgtfy.app/?q=' + query + '&iie=1')
     
     if message.content == '?tone':
-        toneEmb = discord.Embed(title="tone indicators", description="tone indicators are ways to indicate tones in text. \n learn more: https://toneindicators.carrd.co \n usage: ?tone {tone indicator}")
-        await message.channel.send(embed=toneEmb)
+        await message.channel.send(embed=discord.Embed(title="tone indicators", description="tone indicators are ways to indicate tones in text. \n learn more: https://toneindicators.carrd.co \n usage: ?tone {tone indicator}"))
         
     if message.content == '?figurative language':
-        figEmb = discord.Embed(title="figurative language", description="a way of expressing a story/thoughts using words that do not use its strict meaning. \n usage: ?fig lan {type of figurative language}")
-        await message.channel.send(embed=figEmb)
+        await message.channel.send(embed=discord.Embed(title="figurative language", description="a way of expressing a story/thoughts using words that do not use its strict meaning. \n usage: ?fig lan {type of figurative language}"))
         
     if message.content == '?about':
-        aboutEmb = discord.Embed(title="Uranus Bot", description="[a discord bot that was made bcuz i was bored](https://julianagar.github.io/uranusbot-website/) \n version 2.0 (wip) \n get uptime info using ?about")
-        await message.channel.send(embed=aboutEmb)
+        await message.channel.send(embed=discord.Embed(title="Uranus Bot", description="[a discord bot that was made bcuz i was bored](https://julianagar.github.io/uranusbot-website/) \n version 2.0 (wip) \n get uptime info using ?about"))
         
     if message.content == '?time machine': 
-        tmEmb = discord.Embed(title="ten years ago (via neal.fun)", description="websites as they were 10 years ago today. \nvia [neal.fun](https://neal.fun)")
-        await message.channel.send(embed=aboutEmb)
+        await message.channel.send(embed=discord.Embed(title="ten years ago (via neal.fun)", description="websites as they were 10 years ago today. \nvia [neal.fun](https://neal.fun) \nusage: ?time machine [cnn, reddit, nyt, bbc, fox, amazon]"))
         
     if message.content == '?time machine cnn':
-        cnnEmb = discord.Embed(title="CNN 10 years ago today", description="[link](https://ten-years-ago.neal.fun/cnn.com/) \n*oh god rick we're in a discord bot oh god* (wrong show?)")
-        await message.channel.send(embed=aboutEmb)
+        await message.channel.send(embed=discord.Embed(title="CNN 10 years ago today", description="[link](https://ten-years-ago.neal.fun/cnn.com/)"))
         
     if message.content == '?time machine reddit':
-        redEmb = discord.Embed(title="Reddit 10 years ago today", description="[link](https://ten-years-ago.neal.fun/reddit.com/)")
-        await message.channel.send(embed=redEmb)
+        await message.channel.send(embed=discord.Embed(title="Reddit 10 years ago today", description="[link](https://ten-years-ago.neal.fun/reddit.com/)"))
+
+    if message.content == '?time machine nyt':
+       await message.channel.send(embed=discord.Embed(title="New York Times 10 years ago today", description="[link](https://ten-years-ago.neal.fun/nytimes.com/)"))
+
+    if message.content == '?time machine bbc':
+        await message.channel.send(embed=discord.Embed(title="BBC 10 years ago today", description="[link](https://ten-years-ago.neal.fun/bbc.com/)"))
+    
+    if message.content == '?time machine fox':
+        await message.channel.send(embed=discord.Embed(title="Fox News 10 years ago today", description="[link](https://ten-years-ago.neal.fun/foxnews.com/)"))
+
+    if message.content == '?time machine amazon':
+        await message.channel.send(embed=discord.Embed(title="Amazon 10 years ago today", description="[link](https://ten-years-ago.neal.fun/amazon.com/)"))
+
+
 
 async def on_message():
   print(message.content)
